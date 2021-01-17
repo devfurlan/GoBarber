b45a8a0e8fab06ef4bf7d0bd6aaf8c89c0e00071
Lucas Furlan <furlan@hotmail.com.br>
2021-01-17 20:01:25 -0300

    First commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+import { getCustomRepository } from 'typeorm';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({ provider, date: parsedDate });
+
+    return response.json(appointment);
+  } catch (e) {
+    return response.status(400).json({ error: e.message });
+  }
+});
+
+export default appointmentsRouter;
