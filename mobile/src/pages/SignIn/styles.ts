3ac037a1ff7970d2fc790e2f351c17f8bc85c5cf
Lucas Furlan <furlan@hotmail.com.br>
2021-02-20 15:30:58 -0300

    Start mobile

---
+++ mobile/src/pages/SignIn/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 20px;
+  text-align: center;
+  color: #F4EDE8;
+  margin: 64px 0 24px;
+`;
+
